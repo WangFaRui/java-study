be9d8a7ee73c133b6576701d8fe1f221f415b9cc
wangfarui <970201006@qq.com>
2023-02-23 15:39:26 +0800

    Java第五章-流程控制

---
+++ chapter5/ConditionControl.java
@@ -0,0 +1,57 @@
+import java.util.Random;
+
+/**
+ * Java条件控制
+ *
+ * @author wangfarui
+ * @since 2023/2/23
+ */
+public class ConditionControl {
+
+    /**
+     * 通过if判断数值大小
+     *
+     * @param i 数值
+     */
+    public static void judgeNumberByIf(int i) {
+        System.out.print("[judgeNumberByIf]");
+        if (i < 5) {
+            System.out.println("这个数小于5， 它是: " + i);
+        } else if (i < 7) {
+            System.out.println("这个数小于7， 它是: " + i);
+        } else {
+            System.out.println("这个数挺大的，它是:" + i);
+        }
+    }
+
+    /**
+     * 通过switch判断数值大小
+     *
+     * @param i 数值
+     */
+    public static void judgeNumberBySwitch(int i) {
+        System.out.print("[judgeNumberBySwitch]");
+        switch (i) {
+            case 1:
+                System.out.println("它是1");
+                break;
+            case 2:
+                System.out.println("它是2");
+                break;
+            case 3:
+                System.out.println("它是3");
+                break;
+            default:
+                System.out.println("判断不了这个数: " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        // 生成一个随机数
+        Random random = new Random();
+        int i = random.nextInt(10);
+
+        ConditionControl.judgeNumberByIf(i);
+        ConditionControl.judgeNumberBySwitch(i);
+    }
+}
---
+++ chapter5/LoopControl.java
@@ -0,0 +1,83 @@
+import java.util.Random;
+
+/**
+ * 循环控制
+ *
+ * @author wangfarui
+ * @since 2023/2/23
+ */
+public class LoopControl {
+
+    /**
+     * 填充数据数组
+     *
+     * @param arr 数组
+     */
+    public void fillArrayData(int[] arr) {
+        // 定义随机数生成器
+        Random random = new Random();
+        for (int i = 0, len = arr.length; i < len; i++) {
+            // 给数组每个元素都赋随机数值
+            arr[i] = random.nextInt(10);
+        }
+    }
+
+    /**
+     * 使用 for 循环展示
+     *
+     * @param arr 数组数据
+     */
+    public void showByFor(int[] arr) {
+        int n = 0;
+        for (int a : arr) {
+            n++;
+            if (a < 2 || a > 7) {
+                System.out.printf("[judgeNumberByIf][for-break]执行到第%d次，满足if条件的值: %d, 跳出循环。\n", n, a);
+                break;
+            }
+            ConditionControl.judgeNumberByIf(a);
+        }
+    }
+
+    /**
+     * 使用 while 循环展示
+     *
+     * @param arr 数组数据
+     */
+    public void showByWhile(int[] arr) {
+        int i = 0;
+        int len = arr.length;
+        while (i < len) {
+            if (arr[i] < 2 || arr[i] > 7) {
+                System.out.printf("[judgeNumberBySwitch][while-break]执行到第%d次，满足if条件的值: %d, 跳出循环。\n", i + 1, arr[i]);
+                break;
+            }
+            ConditionControl.judgeNumberBySwitch(arr[i]);
+            i++;
+        }
+
+        i = 0;
+        do {
+            if (arr[i] < 2 || arr[i] > 7) {
+                System.out.printf("[judgeNumberByIf][while-continue]执行到第%d次，满足if条件的值: %d, 继续下一次循环而不终止。\n", i + 1, arr[i]);
+                continue;
+            }
+            ConditionControl.judgeNumberByIf(arr[i]);
+        } while (++i < len);
+    }
+
+    public static void main(String[] args) {
+        // 实例化一个 LoopControl 对象
+        LoopControl loopControl = new LoopControl();
+        // 定义一个长度为5的int类型数组
+        int[] arr = new int[5];
+        // 调用 fillArrayData 方法，用于填充数据
+        loopControl.fillArrayData(arr);
+
+        // 使用 for 形式展示
+        loopControl.showByFor(arr);
+
+        // 使用 while 形式展示
+        loopControl.showByWhile(arr);
+    }
+}
