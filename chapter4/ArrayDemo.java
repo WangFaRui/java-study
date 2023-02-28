aed4fafe187d8124431865187f5fa7887803ea28
wangfarui <970201006@qq.com>
2023-02-28 09:37:55 +0800

    Java第四章-完善数组demo

--- chapter4/ArrayDemo.java
+++ chapter4/ArrayDemo.java
@@ -24,16 +24,52 @@ public class ArrayDemo {
      */
     private static int[][][][] FOUR_ARRAY = {{{{1}}}};
 
+
+
+    private static int[][][] DYNAMIC_MULTIPLE_ARRAY = new int[3][3][6];
+
+    public void printMultipleArray() {
+        int[][] a1 = MULTIPLE_ARRAY[0];
+        System.out.println("a1的长度: " + a1.length); // a1的长度: 2
+        int[] a2 = a1[0];
+        System.out.println("a2的长度: " + a2.length); // a2的长度: 4
+        int a3 = a2[0];
+        System.out.println("a3的值: " + a3); // a3的值: 1
+    }
+
+    public void printDynamicMultipleArray() {
+        System.out.println("----------------------");
+        int[][] a1 = DYNAMIC_MULTIPLE_ARRAY[1];
+        System.out.println("a1的长度: " + a1.length); // a1的长度: 3
+        int[] a2 = a1[1];
+        System.out.println("a2的长度: " + a2.length); // a2的长度: 6
+        int a3 = a2[1];
+        System.out.println("a3的值: " + a3); // a3的值: 0
+    }
+
     /**
-     * 打印4维数组
+     * 多维数组
+     */
+    private static int[][][] MULTIPLE_ARRAY = {
+            {
+                    {1, 11, 111, 1111}, {2, 22}
+            },
+            {
+                    {3}
+            },
+            {
+                    {1, 11, 111, 1111}, {2, 22}, {3, 4, 5, 6, 7, 7}
+            }
+    };
+
+    /**
+     * 打印3维数组
      */
     public void print4Array() {
-        for (int[][][] a1 : FOUR_ARRAY) {
-            for (int[][] a2 : a1) {
-                for (int[] a3 : a2) {
-                    for (int a4 : a3) {
-                        System.out.println(a4);
-                    }
+        for (int[][] a2 : MULTIPLE_ARRAY) {
+            for (int[] a3 : a2) {
+                for (int a4 : a3) {
+                    System.out.println(a4);
                 }
             }
         }
@@ -41,11 +77,12 @@ public class ArrayDemo {
 
     /**
      * 打印数组下标
+     * //  private int[] a4 = {1, 2, 3};
      */
     public void printArrayIndex() {
         System.out.println("a4的数组长度: " + a4.length);
         for (int i = 0, size = a4.length; i < size; i++) {
-            System.out.printf("数组下标为[%d]的值为: %d%n", i, a4[i]);
+            System.out.printf("数组下标为a4[%d]的值为: %d%n", i, a4[i]);
         }
     }
 
@@ -54,6 +91,7 @@ public class ArrayDemo {
      */
     public void printArraysFunction() {
         int[] array = {5, 1, 4, 2, 3};
+        System.out.println("直接输出: " + array);
         System.out.println("排序前: " + Arrays.toString(array));
         // 对array排序, 默认升序
         Arrays.sort(array);
@@ -63,6 +101,8 @@ public class ArrayDemo {
 
     public static void main(String[] args) {
         ArrayDemo arrayDemo = new ArrayDemo();
+        arrayDemo.printMultipleArray();
+        arrayDemo.printDynamicMultipleArray();
         arrayDemo.print4Array();
         arrayDemo.printArrayIndex();
         arrayDemo.printArraysFunction();
