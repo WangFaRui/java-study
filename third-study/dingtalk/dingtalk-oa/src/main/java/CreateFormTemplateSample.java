c194e91bdaa013854388f72b20c6190fbcd4e5b4
wangfarui <970201006@qq.com>
2023-08-25 18:03:17 +0800

    钉钉OA审批示例

---
+++ third-study/dingtalk/dingtalk-oa/src/main/java/CreateFormTemplateSample.java
@@ -0,0 +1,42 @@
+import com.alibaba.fastjson.JSON;
+import com.aliyun.tea.*;
+import com.aliyun.teautil.models.*;
+import com.aliyun.dingtalkworkflow_1_0.models.*;
+import com.aliyun.teaopenapi.models.*;
+
+public class CreateFormTemplateSample {
+
+    /**
+     * 使用 Token 初始化账号Client
+     */
+    public static com.aliyun.dingtalkworkflow_1_0.Client createClient() throws Exception {
+        Config config = new Config();
+        config.protocol = "https";
+        config.regionId = "central";
+        return new com.aliyun.dingtalkworkflow_1_0.Client(config);
+    }
+
+    public static void main(String[] args_) throws Exception {
+        com.aliyun.dingtalkworkflow_1_0.Client client = CreateFormTemplateSample.createClient();
+        FormCreateHeaders formCreateHeaders = new FormCreateHeaders();
+        formCreateHeaders.xAcsDingtalkAccessToken = "7011eb7b5a5e37c694b18c6c79406111";
+        // 1. 单行输入控件
+        FormComponentProps formComponentProps1 = new FormComponentProps()
+                .setLabel("名称");
+        FormComponent formComponent1 = new FormComponent()
+                .setComponentType("TextField")
+                .setProps(formComponentProps1);
+
+        FormCreateRequest formCreateRequest = new FormCreateRequest()
+                .setName("表单示例")
+                .setFormComponents(java.util.Arrays.asList(
+                        formComponent1
+                ));
+        try {
+            FormCreateResponse response = client.formCreateWithOptions(formCreateRequest, formCreateHeaders, new RuntimeOptions());
+            System.out.println(JSON.toJSONString(response));
+        } catch (Exception _err) {
+            _err.printStackTrace();
+        }
+    }
+}
---
+++ third-study/dingtalk/dingtalk-oa/src/main/java/StartInstanceSample.java
@@ -0,0 +1,41 @@
+import com.alibaba.fastjson.JSON;
+import com.aliyun.dingtalkworkflow_1_0.models.StartProcessInstanceHeaders;
+import com.aliyun.dingtalkworkflow_1_0.models.StartProcessInstanceRequest;
+import com.aliyun.dingtalkworkflow_1_0.models.StartProcessInstanceResponse;
+import com.aliyun.teaopenapi.models.Config;
+import com.aliyun.teautil.models.RuntimeOptions;
+
+import java.util.Collections;
+
+public class StartInstanceSample {
+
+    /**
+     * 使用 Token 初始化账号Client
+     */
+    public static com.aliyun.dingtalkworkflow_1_0.Client createClient() throws Exception {
+        Config config = new Config();
+        config.protocol = "https";
+        config.regionId = "central";
+        return new com.aliyun.dingtalkworkflow_1_0.Client(config);
+    }
+
+    public static void main(String[] args_) throws Exception {
+        com.aliyun.dingtalkworkflow_1_0.Client client = StartInstanceSample.createClient();
+        StartProcessInstanceHeaders startProcessInstanceHeaders = new StartProcessInstanceHeaders();
+        startProcessInstanceHeaders.xAcsDingtalkAccessToken = "7011eb7b5a5e37c694b18c6c79406111";
+        StartProcessInstanceRequest.StartProcessInstanceRequestFormComponentValues formComponentValues0 = new StartProcessInstanceRequest.StartProcessInstanceRequestFormComponentValues()
+                .setName("名称")
+                .setValue("wray");
+        StartProcessInstanceRequest startProcessInstanceRequest = new StartProcessInstanceRequest()
+                .setOriginatorUserId("306354436429120376")
+                .setProcessCode("PROC-0FAB48EA-FB39-4981-AD80-E67D552B3355")
+                .setDeptId(-1L)
+                .setFormComponentValues(Collections.singletonList(formComponentValues0));
+        try {
+            StartProcessInstanceResponse response = client.startProcessInstanceWithOptions(startProcessInstanceRequest, startProcessInstanceHeaders, new RuntimeOptions());
+            System.out.println(JSON.toJSONString(response));
+        } catch (Exception _err) {
+            _err.printStackTrace();
+        }
+    }
+}
