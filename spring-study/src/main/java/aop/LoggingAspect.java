8dd66daa53db3c0378f67cbdf4f2611bb3b4f66b
wangfarui <970201006@qq.com>
2023-09-19 18:08:38 +0800

    aop variable problem

---
+++ spring-study/src/main/java/aop/LoggingAspect.java
@@ -0,0 +1,21 @@
+package aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+/**
+ * 日志 AOP
+ *
+ * @author Wray
+ * @since 2023/9/19
+ */
+@Aspect
+@Component
+public class LoggingAspect {
+
+    @Before("execution(* aop.UserService.*(..))")
+    public void printLog() {
+        System.out.println("[Before] print log");
+    }
+}
---
+++ spring-study/src/main/java/aop/SpringAopVariableDemo.java
@@ -0,0 +1,41 @@
+package aop;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+/**
+ * spring aop 成员变量示例
+ *
+ * @author Wray
+ * @since 2023/9/19
+ */
+@ComponentScan
+@EnableAspectJAutoProxy
+public class SpringAopVariableDemo {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+        applicationContext.register(SpringAopVariableDemo.class);
+        applicationContext.refresh();
+
+        UserService userService = applicationContext.getBean(UserService.class);
+
+        ZoneId serviceZoneId = userService.getZoneId();
+        System.out.println("serviceZoneId: " + ZonedDateTime.now(serviceZoneId));
+
+        ZoneId finalZoneId = userService.getFinalZoneId();
+        if (finalZoneId != null) {
+            System.out.println("finalZoneId: " + ZonedDateTime.now(finalZoneId));
+        }
+
+        ZoneId zoneId = userService.zoneId;
+        if (zoneId != null) {
+            System.out.println("zoneId: " + ZonedDateTime.now(zoneId));
+        }
+
+    }
+}
