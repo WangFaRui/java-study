31a14d11e98cfa5901b40058141099369acde120
Wray <wray20156294@gmail.com>
2024-06-12 16:10:32 +0800

    Java值传递

---
+++ advance-study/src/main/java/com/itwray/study/advance/reference/Person.java
@@ -0,0 +1,31 @@
+package com.itwray.study.advance.reference;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        Person xiaoZhang = new Person("小张");
+        Person xiaoLi = new Person("小李");
+        swap(xiaoZhang, xiaoLi);
+        System.out.println("xiaoZhang:" + xiaoZhang.getName());
+        System.out.println("xiaoLi:" + xiaoLi.getName());
+    }
+
+    public static void swap(Person person1, Person person2) {
+        Person temp = person1;
+        person1 = person2;
+        person2 = temp;
+        System.out.println("person1:" + person1.getName());
+        System.out.println("person2:" + person2.getName());
+    }
+
+}
+
