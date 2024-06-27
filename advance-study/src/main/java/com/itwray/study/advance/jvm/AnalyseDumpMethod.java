94b7a67501c684a0d356748afb4071df6c02ef3b
Wray <wray20156294@gmail.com>
2024-06-27 18:05:34 +0800

    dump file analyse tool

---
+++ advance-study/src/main/java/com/itwray/study/advance/jvm/AnalyseDumpMethod.java
@@ -0,0 +1,13 @@
+package com.itwray.study.advance.jvm;
+
+/**
+ * 分析 dump 文件的方法
+ * <p>
+ * 下载 Memory Analyzer Tool 工具，简称 MAT。 <p>
+ *
+ * @author wray
+ * @since 2024/6/27
+ */
+public class AnalyseDumpMethod {
+
+}
